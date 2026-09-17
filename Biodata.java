db47fc7f3b9eabaec3bf534996595dbefc3cd645
Fheelc <depankiti@gmail.com>
2026-09-17 15:35:13 +0700

    Add files via upload

---
+++ Biodata.java
@@ -0,0 +1,18 @@
+public class Main {
+  public static void main(String[] args) {
+    String nama = "Mohamad Faisal Apriansyah";
+    int Nim = 261301012;
+    String Alamat = "Margorejo, Parengan, Tuban";
+    int KodePos = 62366;
+    String Fakultas = "Fakultas Sains dan Teknologi";
+    String Prodi = "Sistem Komputer";
+    String Hobi = "Nge Game";
+    System.out.println("Nama	: "+nama);
+    System.out.println("Nim	: "+Nim);
+    System.out.println("Alamat	: "+Alamat);
+    System.out.println("KodePos	:"+KodePos);
+    System.out.println("Fakultas:"+Fakultas);
+    System.out.println("Prodi :"+Prodi);
+    System.out.println("Hobi	:"+Hobi);
+  }
+}
